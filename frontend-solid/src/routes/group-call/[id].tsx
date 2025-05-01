f1bbfd16fa7757f4b2e29c801eb22242d9338049
kyrylosheliah <kyrylosheliah@gmail.com>
2025-05-01 19:54:36 +0300

    implement leaving call action

--- frontend-solid/src/routes/group-call/[id].tsx
+++ frontend-solid/src/routes/group-call/[id].tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "@solidjs/router";
 import GroupCall from "~/components/GroupCall";
 
 export default function GroupCallRoom() {
+  const navigate = useNavigate();
+  const leave = () => {
+    navigate(-1);
+  };
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <div><button onClick={leave}>Leave</button></div>
       <GroupCall />
     </main>
   );
